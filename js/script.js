3ded1476d10fd64de1447775eb6adaa3b006c878
@Jeremyobrien <jjob1391@gmail.com>
2021-06-22 16:08:06 -0400

    Fixed output to the DOM

--- js/script.js
+++ js/script.js
@@ -4,73 +4,62 @@ FSJS Project 2 - list Pagination and Filtering
 */
 
 
-/*
-For assistance:
-   Check out the "Project Resources" section of the Instructions tab: https://teamtreehouse.com/projects/list-pagination-and-filtering#instructions
-   Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
-*/
-
-
-
-/*
-Create the `showPage` function
-This function will create and insert/append the elements needed to display a "page" of nine students
-*/
-function showPage (list, page) {
-   let startIndex = (page * 9) - 9;
-   let endIndex = page * 9;
-   let studentList = document.querySelector('.student-list');
+//This function will create and insert/append the elements needed to display a "page" of nine students
+function showPage(list, page) {
+   const startIndex = (page * 9) - 9;
+   const endIndex = page * 9;
+   const studentList = document.querySelector('.student-list');
    studentList.innerHTML = '';
    for (let i = 0; i < list.length; i++) {
-      if (list[i] >= startIndex && list[i] < endIndex) {
-         
-         const studentCard = `<li class="student-item cf">
+       if (i >= startIndex && i < endIndex) { 
+         const studentName = `${list[i].name.first} ${list[i].name.last}`;
+         const studentPic = list[i].picture.large;
+         const studentEmail = list[i].email;
+         const joinedDate = list[i].registered.date;
+         let studentItem = `<li class="student-item cf">
                                  <div class="student-details">
-                                    <img class="avatar" src=${list.picture.thumbnail} alt="Profile Picture">
-                                    <h3>${list.name.first} ${list.name.last}</h3>
-                                    <span class="email">${list.email}</span>
+                                    <img class="avatar" src=${studentPic} alt="Profile Picture">
+                                    <h3>${studentName}</h3>
+                                    <span class="email">${studentEmail}</span>
                                  </div>
                                  <div class="joined-details">
-                                    <span class="date">Joined ${list.registered.date}</span>
+                                    <span class="date">Joined ${joinedDate}</span>
                                  </div>
                               </li>`
-         studentList.insertAdjacentHTML('beforeend', studentCard);
+         studentList.insertAdjacentHTML('beforeend', studentItem);
       }
    }
-   return studentList;
-};
+}
 
+// /*
+// Create the `addPagination` function
+// This function will create and insert/append the elements needed for the pagination buttons
+// */
 
-/*
-Create the `addPagination` function
-This function will create and insert/append the elements needed for the pagination buttons
-*/
-
-function createPageButtons(list) {
-   const linkList = document.querySelector('.link-list');
-   const numOfButtons = list.length/9;
-   linkList.innerHTML = '';
-   for(let i = 0; i < numOfButtons; i++) {
-       let pageButton = document.createElement('button');
-            pageButton += `<li>
-                           <button type='button'>${pageButton.textContent = i+1}</button>
+function addPagination(list) {
+      const numOfPages = Math.ceil(list.length/9);
+      const linkList = document.querySelector('.link-list');
+   for(let i = 1; i <= numOfPages; i++) {
+      let pageButton = document.createElement('button');
+            pageButton = `<li>
+                           <button type='button'>${pageButton.textContent = i}</button>
                          </li>`
       linkList.insertAdjacentHTML('beforeend', pageButton);
    }
-   linkList.firstElementChild.className('active');
-   linkList.addEventListener('click', (e)=> {
-      if (e.target.type === 'button') {
-         for (let i = 0; i < linkList.length; i++) {
-            linkList[i].classList.remove('active');
-         }
-         e.target.classList.add('active');
-         showPage(list, e.target.textContent);
+      linkList.firstElementChild.className = 'active';
 
-      }
-   })
+      linkList.addEventListener('click', (e)=> {
+         if (e.target.tagName === 'BUTTON') {
+            let currentActiveButton = document.querySelector('.active');
+                currentActiveButton.className = '';
+                e.target.className = 'active';
+               showPage(list, e.target.textContent);
+
+         }
+      })
 }
 
-// Call functions
 
-showPage(Data, 1);
-createPageButtons(Data);
+// // Call functions
+showPage(data, 1);
+addPagination(data);
