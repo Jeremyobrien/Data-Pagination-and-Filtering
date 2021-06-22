ae7ad523a8d9684b8c40a9567fa07bc83fb7276d
@Jeremyobrien <jjob1391@gmail.com>
2021-06-22 16:27:24 -0400

    Updated comments

--- js/script.js
+++ js/script.js
@@ -6,10 +6,13 @@ FSJS Project 2 - list Pagination and Filtering
 
 //This function will create and insert/append the elements needed to display a "page" of nine students
 function showPage(list, page) {
+   //Calculate and store first and last indexes for each page and access UL with class 'student-list'
    const startIndex = (page * 9) - 9;
    const endIndex = page * 9;
    const studentList = document.querySelector('.student-list');
+   //Clear any pre-existing page results
    studentList.innerHTML = '';
+   //Access each student object and create a 'studentItem' card with student data that is inserted into 'studentList'
    for (let i = 0; i < list.length; i++) {
        if (i >= startIndex && i < endIndex) { 
          const studentName = `${list[i].name.first} ${list[i].name.last}`;
@@ -31,30 +34,29 @@ function showPage(list, page) {
    }
 }
 
-// /*
-// Create the `addPagination` function
-// This function will create and insert/append the elements needed for the pagination buttons
-// */
 
+// This function will create and insert/append the elements needed for the pagination buttons
 function addPagination(list) {
-      const numOfPages = Math.ceil(list.length/9);
-      const linkList = document.querySelector('.link-list');
+   //Stores number of pages needed for dataset and accesses DOM element for page buttons
+   const numOfPages = Math.ceil(list.length/9);
+   const linkList = document.querySelector('.link-list');
+   //Creates apporpriate number of page buttons and inserts them into DOM
    for(let i = 1; i <= numOfPages; i++) {
       let pageButton = document.createElement('button');
-            pageButton = `<li>
-                           <button type='button'>${pageButton.textContent = i}</button>
-                         </li>`
+          pageButton = `<li>
+                        <button type='button'>${pageButton.textContent = i}</button>
+                        </li>`
       linkList.insertAdjacentHTML('beforeend', pageButton);
    }
+      //Set first page as default 'active' page
       linkList.firstElementChild.className = 'active';
-
+      //Listen for page button clicks to change page results and 'active' status
       linkList.addEventListener('click', (e)=> {
          if (e.target.tagName === 'BUTTON') {
             let currentActiveButton = document.querySelector('.active');
                 currentActiveButton.className = '';
                 e.target.className = 'active';
                showPage(list, e.target.textContent);
-
          }
       })
 }
