9f90d7be4ffd5067528539f8d5f80f7ba7f102e8
@Jeremyobrien <jjob1391@gmail.com>
2021-06-24 21:44:34 -0400

    Added searchFunction

--- js/script.js
+++ js/script.js
@@ -3,12 +3,11 @@ Treehouse Techdegree:
 FSJS Project 2 - list Pagination and Filtering
 */
 const header = document.querySelector('.header');
-
 header.insertAdjacentHTML('beforeend',
    `<label for="search" class="student-search">
    <span>Search by name</span>
    <input id="search" placeholder="Search by name...">
-   <button type="button"><img src="img/icn-search.svg" alt="Search icon"></button>
+   <button type="button" id="search-button"><img src="img/icn-search.svg" alt="Search icon"></button>
  </label>`
 )
 
@@ -18,7 +17,7 @@ function showPage(list, page) {
    const startIndex = (page * 9) - 9;
    const endIndex = page * 9;
    const studentList = document.querySelector('.student-list');
-   //Clear any pre-existing page results
+   //Clear any pre-existing page results 
    studentList.innerHTML = '';
    //Access each student object and create a 'studentItem' card with student data that is inserted into 'studentList'
    for (let i = 0; i < list.length; i++) {
@@ -48,10 +47,11 @@ function addPagination(list) {
    //Stores number of pages needed for dataset and accesses DOM element for page buttons
    const numOfPages = Math.ceil(list.length/9);
    const linkList = document.querySelector('.link-list');
+   linkList.innerHTML = '';
    //Creates apporpriate number of page buttons and inserts them into DOM
    for(let i = 1; i <= numOfPages; i++) {
-      let pageButton = document.createElement('button');
-          pageButton = `<li>
+         let pageButton = document.createElement('button'); 
+             pageButton =`<li>
                         <button type='button'>${pageButton.textContent = i}</button>
                         </li>`
       linkList.insertAdjacentHTML('beforeend', pageButton);
@@ -69,8 +69,47 @@ function addPagination(list) {
       })
 }
 
+const search = document.querySelector('#search');
+const searchButton = document.querySelector('#search-button');
 
 
-// // Call functions
+const searchFunction= (searchInput, list)=> {
+   let filteredList = [];
+   
+   for (let i=0; i < list.length; i++) {
+     let student = list[i];
+      searchInput = search.value;
+     let studentName = `${student.name.first.toLowerCase()} ${student.name.last.toLowerCase()}`;
+      if (searchInput.length !== 0 && studentName.includes(searchInput.toLowerCase())) {
+         filteredList.push(student);
+      } else if (searchInput.length === 0) {
+         filteredList = data;
+      } else if (searchInput.length !== 0 && !studentName.includes(searchInput.toLowerCase())){
+         let studentList = document.querySelector('.student-list');
+         studentList.className = 'no-results';
+         studentList.innerHTML = '<h2>Sorry, there are no students with that name.</h2>'
+      }
+      }
+   
+      showPage(filteredList, 1);
+      console.log(searchInput);
+      addPagination(filteredList);
+   }
+
+
+
+search.addEventListener('keyup', (e)=> {
+   e.preventDefault();
+   searchFunction(e.target.value, data);
+});
+
+searchButton.addEventListener('click', (e)=> {
+   e.preventDefault();
+   searchFunction(search.value, data);
+});
+
+
+
+// // // Call functions
 showPage(data, 1);
 addPagination(data);
