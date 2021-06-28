d63847ab67a07494b8cade203ecf619e3b898a5b
@Jeremyobrien <jjob1391@gmail.com>
2021-06-28 16:48:24 -0400

    Added comments and formatting

--- js/script.js
+++ js/script.js
@@ -3,7 +3,11 @@ Treehouse Techdegree:
 FSJS Project 2 - list Pagination and Filtering
 */
 
+//Global variables
 const header = document.querySelector('.header');
+const studentList = document.querySelector('.student-list');
+
+//Inserts search bar and button in to DOM
 header.insertAdjacentHTML('beforeend',
    `<label for="search" class="student-search">
    <span>Search by name</span>
@@ -11,13 +15,12 @@ header.insertAdjacentHTML('beforeend',
    <button type="button" id="search-button"><img src="img/icn-search.svg" alt="Search icon"></button>
  </label>`
 )
-const studentList = document.querySelector('.student-list');
+
 //This function will create and insert/append the elements needed to display a "page" of nine students
 function showPage(list, page) {
    //Calculate and store first and last indexes for each page and access UL with class 'student-list'
    const startIndex = (page * 9) - 9;
-   const endIndex = page * 9;
-   
+   const endIndex = page * 9;  
    //Clear any pre-existing page results 
    studentList.innerHTML = '';
    //Access each student object and create a 'studentItem' card with student data that is inserted into 'studentList'
@@ -51,8 +54,7 @@ function addPagination(list) {
    linkList.innerHTML = '';
    //Creates apporpriate number of page buttons and inserts them into DOM
    for(let i = 1; i <= numOfPages; i++) {
-      //Set first page as default 'active' page
-      
+      //Set first page as default 'active' page      
       if (numOfPages > 0) {
          let pageButton = document.createElement('button'); 
              pageButton =`<li>
@@ -64,7 +66,7 @@ function addPagination(list) {
       } 
    }
       
-      //Listen for page button clicks to change page results and 'active' status
+      //Listens for page button clicks to change page results and 'active' button status
       linkList.addEventListener('click', (e)=> {
          if (e.target.tagName === 'BUTTON') {
             let currentActiveButton = document.querySelector('.active');
@@ -75,36 +77,39 @@ function addPagination(list) {
       })
 }
 
+//'Search-Scope' variables
 const search = document.querySelector('#search');
 const searchButton = document.querySelector('#search-button');
-
-
+//Shows filtered search results from student dataset or error message according to search input
 const searchFunction = (searchInput, list) => {
    let filteredList = [];
+   //loops through dataset and pushes potential search matches to 'filteredList'
    for (let i = 0; i < list.length; i++) {
      let student = list[i];
       searchInput = search.value.toLowerCase();
      let studentName = `${student.name.first.toLowerCase()} ${student.name.last.toLowerCase()}`;
-      if (searchInput.length !== 0 && studentName.includes(searchInput)) {
+         if (searchInput.length !== 0 && studentName.includes(searchInput)) {
          filteredList.push(student);
+       }
       }
       showPage(filteredList, 1);
-      addPagination(filteredList);
-      }    
+      addPagination(filteredList);  
+   //returns error message if there are no matches
    if (filteredList.length === 0) {
          studentList.innerHTML = '';
          const errorMessage = '<p class="no-results">Sorry, there are no students with that name.</p>';
          studentList.insertAdjacentHTML('beforeend', errorMessage);
        } 
-      }
+}
 
 
-
+//Event listeners that handle search input
 search.addEventListener('keyup', (e)=> {
    e.preventDefault();
    searchFunction(e.target.value, data);
 });
 
+//handles 'search button' clicks tied to search input field
 searchButton.addEventListener('click', (e)=> {
    e.preventDefault();
    searchFunction(search.value, data);
@@ -112,6 +117,6 @@ searchButton.addEventListener('click', (e)=> {
 
 
 
-// // // Call functions
+//Call functions
 showPage(data, 1);
 addPagination(data);
