a1327e9f023ed37e896da892b3eed87379cea163
@Jeremyobrien <jjob1391@gmail.com>
2021-06-28 15:34:15 -0400

    Fixed 'searchFunction()' conditional fltering

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@
 Treehouse Techdegree:
 FSJS Project 2 - list Pagination and Filtering
 */
+
 const header = document.querySelector('.header');
 header.insertAdjacentHTML('beforeend',
    `<label for="search" class="student-search">
@@ -10,13 +11,13 @@ header.insertAdjacentHTML('beforeend',
    <button type="button" id="search-button"><img src="img/icn-search.svg" alt="Search icon"></button>
  </label>`
 )
-
+const studentList = document.querySelector('.student-list');
 //This function will create and insert/append the elements needed to display a "page" of nine students
 function showPage(list, page) {
    //Calculate and store first and last indexes for each page and access UL with class 'student-list'
    const startIndex = (page * 9) - 9;
    const endIndex = page * 9;
-   const studentList = document.querySelector('.student-list');
+   
    //Clear any pre-existing page results 
    studentList.innerHTML = '';
    //Access each student object and create a 'studentItem' card with student data that is inserted into 'studentList'
@@ -50,17 +51,19 @@ function addPagination(list) {
    linkList.innerHTML = '';
    //Creates apporpriate number of page buttons and inserts them into DOM
    for(let i = 1; i <= numOfPages; i++) {
+      //Set first page as default 'active' page
+      
       if (numOfPages > 0) {
          let pageButton = document.createElement('button'); 
              pageButton =`<li>
                         <button type='button'>${pageButton.textContent = i}</button>
                         </li>`
-      linkList.insertAdjacentHTML('beforeend', pageButton);
-      }
+         linkList.insertAdjacentHTML('beforeend', pageButton);
+         let defaultActivePage = linkList.firstElementChild.firstElementChild;
+         defaultActivePage.className = 'active';
+      } 
    }
-      //Set first page as default 'active' page
-      let defaultActivePage = linkList.firstElementChild.firstElementChild;
-      defaultActivePage.className = 'active';
+      
       //Listen for page button clicks to change page results and 'active' status
       linkList.addEventListener('click', (e)=> {
          if (e.target.tagName === 'BUTTON') {
@@ -76,30 +79,24 @@ const search = document.querySelector('#search');
 const searchButton = document.querySelector('#search-button');
 
 
-const searchFunction= (searchInput, list)=> {
+const searchFunction = (searchInput, list) => {
    let filteredList = [];
-   
-   for (let i=0; i < list.length; i++) {
+   for (let i = 0; i < list.length; i++) {
      let student = list[i];
-      searchInput = search.value;
+      searchInput = search.value.toLowerCase();
      let studentName = `${student.name.first.toLowerCase()} ${student.name.last.toLowerCase()}`;
-      if (searchInput.length !== 0 && studentName.includes(searchInput.toLowerCase())) {
+      if (searchInput.length !== 0 && studentName.includes(searchInput)) {
          filteredList.push(student);
-      } else if (searchInput.length === 0) {
-         filteredList = data;
-      } else if (searchInput.length !== 0 && !studentName.includes(searchInput.toLowerCase())){
-         let studentList = document.querySelector('.student-list');
-         let errorMessage = document.createElement('p');
-         errorMessage.className = 'no-results';
-         errorMessage.innerHTML = '<p>Sorry, there are no students with that name.<p>';
-         studentList.insertAdjacentHTML('beforeend', errorMessage);
-       }
       }
-   
       showPage(filteredList, 1);
-      console.log(searchInput);
       addPagination(filteredList);
-   }
+      }    
+   if (filteredList.length === 0) {
+         studentList.innerHTML = '';
+         const errorMessage = '<p class="no-results">Sorry, there are no students with that name.</p>';
+         studentList.insertAdjacentHTML('beforeend', errorMessage);
+       } 
+      }
 
 
 
